c8b882dbfe4444c03b392a24591553c1d113aab4
abcdlsj <lisongjianshuai@gmail.com>
2020-06-04 00:38:50 +0800

    add noncopyable.h

---
+++ Aurora/Callbacks.h
@@ -0,0 +1,8 @@
+//
+// Created by abcdlsj on 2020/6/4.
+//
+
+#ifndef AURORA_CALLBACKS_H
+#define AURORA_CALLBACKS_H
+
+#endif //AURORA_CALLBACKS_H
---
+++ Aurora/ThreadPool.h
@@ -0,0 +1,17 @@
+//
+// Created by abcdlsj on 2020/6/3.
+//
+
+#ifndef AURORA_THREADPOOL_H
+#define AURORA_THREADPOOL_H
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <thread>
+#include <vector>
+#include <deque>
+
+#include "noncopyable.h"
+#include "Callbacks.h"
+
+#endif //AURORA_THREADPOOL_H
